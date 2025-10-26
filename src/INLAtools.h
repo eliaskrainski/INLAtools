2fe83fd75a65503a8e9dd2996e8eced4c3ecab4d
Elias T Krainski <eliaskrainski@gmail.com>
2025-10-26 15:35:35 +0300

    use defined(INLA_EXTERNAL_PACKAGE)

--- src/INLAtools.h
+++ src/INLAtools.h
@@ -71,7 +71,10 @@ typedef int fortran_charlen_t;
 #endif
 #define F_ONE ((fortran_charlen_t)1)
 
+
+#if !defined(INLA_EXTERNAL_PACKAGE)
 SEXP inla_cgeneric_element_get(SEXP Rcmd, SEXP Stheta, SEXP Sntheta, SEXP ints,
 			       SEXP doubles, SEXP chars, SEXP mats, SEXP smats);
+#endif
 inla_cgeneric_func_tp inla_cgeneric_generic0;
 inla_cgeneric_func_tp inla_cgeneric_kronecker;
