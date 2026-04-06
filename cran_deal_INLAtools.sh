37796e1c8f5a451970410ce54a3567103a57a1fd
Elias T Krainski <eliaskrainski@gmail.com>
2026-04-06 10:48:13 +0300

    mk

--- cran_deal_INLAtools.sh
+++ cran_deal_INLAtools.sh
@@ -1,12 +1,13 @@
 rm -rf ~/temp/INLAtools*
 mkdir ~/temp/INLAtools/
 cp -r DESCRIPTION NAMESPACE R/ man/ src/ demo/ ~/temp/INLAtools/
-cd vignettes/
-mkdir ~/temp/INLAtools/vignettes/
-cp cgenericKronecker.pdf cgenericKronecker.pdf.asis ~/temp/INLAtools/vignettes/
-cd ~/temp/INLAtools/vignettes/
-R -e "tools::compactPDF('cgenericKronecker.pdf', gs_quality = 'ebook')"
-cd ../../
+#cd vignettes/
+#mkdir ~/temp/INLAtools/vignettes/
+#cp cgenericKronecker.pdf cgenericKronecker.pdf.asis ~/temp/INLAtools/vignettes/
+#cd ~/temp/INLAtools/vignettes/
+#R -e "tools::compactPDF('cgenericKronecker.pdf', gs_quality = 'ebook')"
+#cd ../../
+cd ~/temp/
 R CMD build INLAtools
 R CMD check INLAtools_*.tar.gz --as-cran
 
