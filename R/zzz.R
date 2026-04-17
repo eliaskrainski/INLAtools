1f8e99d499f835340e6090d20fe9292881f214c6
Elias T Krainski <eliaskrainski@gmail.com>
2026-04-17 10:08:03 +0200

    updt zzz,Makevars

--- R/zzz.R
+++ R/zzz.R
@@ -1,22 +1,7 @@
 .onAttach <- function(libname, pkgname) {
-  if(.Platform$OS.type != "windows") {
-    bINLA <- paste0(find.package("INLA"), "/bin/", apINLAbin())
-    if((!is.na(file.info("/Library")$isdir)) &&
-       (!is.na(file.info("/Applications")$isdir))) {
-    ##  Sys.getenv("DYLD_LIBRARY_PATH")
-      Sys.setenv(DYLD_LIBRARY_PATH=paste0(bINLA,
-                                          ":",
-                                          Sys.getenv("DYLD_LIBRARY_PATH"))
-                 )
-    ##  Sys.getenv("DYLD_LIBRARY_PATH")
-    } else {
-##      print(Sys.getenv("LD_LIBRARY_PATH"))
-      Sys.setenv(LD_LIBRARY_PATH = paste0(bINLA,
-                                          ":",
-                                          Sys.getenv("LD_LIBRARY_PATH"))
-                 )
-  ##    Sys.getenv("LD_LIBRARY_PATH")
-    }
-  }
+  packageStartupMessage(
+   paste("Welcome to the INLAtools package!\n",
+         "More at eliaskrainski.github.io/INLAtools")
+  )
 }
 
